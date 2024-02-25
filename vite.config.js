c4d5b584aa6a316e9dc5c2b1611aa422a4c52984
Yukihan <yukanzen@hotmail.com>
2024-02-25 15:57:20 -0300

    adding to deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Login-Page",
+  base: "https://yukihanz.github.io/Login-Page/",
   resolve: {
     alias: {
       src: "/src",
